2206ff1c4d0688bdb08c78d1c90abdce270aa85c
Alexander Watts <alexanderwatts@Alexanders-MacBook-Pro.local>
2024-10-28 18:52:38 +0000

    Create token position and add it to token

--- src/token/mod.rs
+++ src/token/mod.rs
@@ -1,3 +1,4 @@
 pub mod token;
 pub mod token_literal;
+pub mod token_position;
 pub mod token_type;
--- src/token/token.rs
+++ src/token/token.rs
@@ -1,22 +1,31 @@
-use super::{token_literal::TokenLiteral, token_type::TokenType};
+use super::{token_literal::TokenLiteral, token_position::TokenPosition, token_type::TokenType};
 
+#[derive(Debug)]
 pub struct Token {
     token_type: TokenType,
     token_literal: TokenLiteral,
+    token_position: TokenPosition,
 }
 
 impl Token {
-    pub fn new(token_type: TokenType, token_literal: TokenLiteral) -> Self {
+    pub fn new(
+        token_type: TokenType,
+        token_literal: TokenLiteral,
+        token_position: TokenPosition,
+    ) -> Self {
         Self {
             token_type,
             token_literal,
+            token_position,
         }
     }
 }
 
 #[cfg(test)]
 mod token_tests {
-    use crate::token::{token_literal::TokenLiteral, token_type::TokenType};
+    use crate::token::{
+        token_literal::TokenLiteral, token_position::TokenPosition, token_type::TokenType,
+    };
 
     use super::Token;
 
@@ -25,6 +34,7 @@ mod token_tests {
         let t1 = Token::new(
             TokenType::String,
             TokenLiteral::String("Hello, World!".to_string()),
+            TokenPosition::new(1, 1, 1),
         );
 
         assert_eq!(TokenType::String, t1.token_type);
@@ -32,5 +42,6 @@ mod token_tests {
             TokenLiteral::String("Hello, World!".to_string()),
             t1.token_literal
         );
+        assert_eq!(TokenPosition::new(1, 1, 1), t1.token_position)
     }
 }
