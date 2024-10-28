45e929ab3239a2b22bb99c91ddd916c16925d3aa
Alexander Watts <alexanderwatts@Alexanders-MacBook-Pro.local>
2024-10-28 19:26:11 +0000

    Update token docs

--- src/token/token.rs
+++ src/token/token.rs
@@ -1,5 +1,25 @@
 use super::{token_literal::TokenLiteral, token_position::TokenPosition, token_type::TokenType};
 
+///Encapsulate scanned words
+///
+///A token is a wrapper over some scanned word as a result of lexical analysis providing
+///information such as its type, literal value and position.
+///
+///# Examples
+///
+///```
+///let t1 = Token::new(
+///    TokenType::String,
+///    TokenLiteral::String("Hello, World!".to_string()),
+///    TokenPosition::new(1, 1, 15),
+///);
+///
+///let t2 = Token::new(
+///    TokenType::String,
+///    TokenLiteral::Null,
+///    TokenPosition::new(1, 1, 4),
+///);
+///```
 #[derive(Debug)]
 pub struct Token {
     pub token_type: TokenType,
@@ -34,7 +54,7 @@ mod token_tests {
         let t1 = Token::new(
             TokenType::String,
             TokenLiteral::String("Hello, World!".to_string()),
-            TokenPosition::new(1, 1, 1),
+            TokenPosition::new(1, 1, 15),
         );
 
         assert_eq!(TokenType::String, t1.token_type);
@@ -42,6 +62,6 @@ mod token_tests {
             TokenLiteral::String("Hello, World!".to_string()),
             t1.token_literal
         );
-        assert_eq!(TokenPosition::new(1, 1, 1), t1.token_position)
+        assert_eq!(TokenPosition::new(1, 1, 15), t1.token_position)
     }
 }
