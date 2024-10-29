5141bc2c7abd3481577ceae6a537930a7619f5e1
Alexander Watts <alexanderwatts@Alexanders-MacBook-Pro.local>
2024-10-29 16:38:54 +0000

    Add new scanner error for unterminated strings

--- src/scanner/scanner_error.rs
+++ src/scanner/scanner_error.rs
@@ -3,12 +3,14 @@ use std::{error::Error, fmt::Display};
 #[derive(Debug, PartialEq)]
 pub enum ScannerError {
     UnknownCharacter,
+    UnterminatedString,
 }
 
 impl Display for ScannerError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::UnknownCharacter => write!(f, "Unknown character"),
+            Self::UnterminatedString => write!(f, "Unterminated string"),
         }
     }
 }
@@ -20,10 +22,18 @@ mod scanner_error {
     use super::ScannerError;
 
     #[test]
-    fn scanner_error() {
+    fn unknown_character() {
         let unknown: Result<&str, ScannerError> = Err(ScannerError::UnknownCharacter);
 
         assert_eq!(Err(ScannerError::UnknownCharacter), unknown);
         assert_eq!("Unknown character", unknown.unwrap_err().to_string());
+    }
+
+    #[test]
+    fn unterminated_string() {
+        let unterminated: Result<&str, ScannerError> = Err(ScannerError::UnterminatedString);
+
+        assert_eq!(Err(ScannerError::UnterminatedString), unterminated);
+        assert_eq!("Unknown character", unterminated.unwrap_err().to_string());
     }
 }
