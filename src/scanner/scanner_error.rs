086556975a63b98d9e6c4969422aad568ca2c025
Alexander Watts <alexanderwatts@Alexanders-MacBook-Pro.local>
2024-10-29 10:15:46 +0000

    Created custom scanner error

---
+++ src/scanner/scanner_error.rs
@@ -0,0 +1,29 @@
+use std::{error::Error, fmt::Display};
+
+#[derive(Debug, PartialEq)]
+pub enum ScannerError {
+    UnknownCharacter,
+}
+
+impl Display for ScannerError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::UnknownCharacter => write!(f, "Unknown character"),
+        }
+    }
+}
+
+impl Error for ScannerError {}
+
+#[cfg(test)]
+mod scanner_error {
+    use super::ScannerError;
+
+    #[test]
+    fn scanner_error() {
+        let unknown: Result<&str, ScannerError> = Err(ScannerError::UnknownCharacter); 
+       
+        assert_eq!(Err(ScannerError::UnknownCharacter), unknown);
+        assert_eq!("Unknown character", unknown.unwrap_err().to_string());
+    }
+}
