6b6579cbdad96bb18c31c776be3491b546ad1ef2
Alexander Watts <alexanderwatts@Alexanders-MacBook-Pro.local>
2024-10-31 13:46:54 +0000

    Look at the next token in the parser

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -10,7 +10,37 @@ impl<'a> Parser {
     pub fn new(tokens: Vec<Token>) -> Self {
         Self { current: 0, tokens }
     }
+
+    fn peek(&self) -> Option<&Token> {
+        self.tokens.get(self.current)
+    }
 }
 
 #[cfg(test)]
-mod parser_tests {}
+mod parser_tests {
+    use crate::token::{
+        token::Token, token_literal::TokenLiteral, token_position::TokenPosition,
+        token_type::TokenType,
+    };
+
+    use super::Parser;
+
+    #[test]
+    fn get_current_token() {
+        let parser = Parser::new(vec![Token::new(
+            TokenType::True,
+            TokenLiteral::Bool(true),
+            TokenPosition::new(1, 1, 2),
+        )]);
+        let token = parser.peek().unwrap();
+
+        assert_eq!(
+            Token::new(
+                TokenType::True,
+                TokenLiteral::Bool(true),
+                TokenPosition::new(1, 1, 2),
+            ),
+            *token
+        );
+    }
+}
