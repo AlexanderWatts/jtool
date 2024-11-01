3b1fecb1f0e08707996f9fb1b86db615eb9ffd5c
Alexander Watts <alexanderwatts@Alexanders-MacBook-Pro.local>
2024-11-01 17:15:22 +0000

    Changed parser error unknown value to unexpected token passing a string

--- src/parser/parser_error.rs
+++ src/parser/parser_error.rs
@@ -2,13 +2,13 @@ use std::{error::Error, fmt::Display};
 
 #[derive(Debug, PartialEq)]
 pub enum ParserError {
-    UnknownValue,
+    UnexpectedToken(String),
 }
 
 impl Display for ParserError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::UnknownValue => write!(f, ""),
+            Self::UnexpectedToken(error_message) => write!(f, "{}", error_message),
         }
     }
 }
